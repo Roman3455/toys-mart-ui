9a71c8d0251f6c31385e6434dca59439cde04c81
Roman Smirnov <s.roman3455@gmail.com>
2026-01-20 23:23:53 +0300

    feat: add Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import Header from "./components/Header.jsx";
+import Footer from "./components/footer/Footer.jsx";
 
 function App() {
   return (
-    <Header />
+    <>
+      <Header />
+      <Footer />
+    </>
   );
 }
 
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,20 @@
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faHeart} from '@fortawesome/free-solid-svg-icons';
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      Built with
+      <FontAwesomeIcon
+        icon={faHeart}
+        className="footer-icon"
+        aria-hidden="true"
+      />
+       by
+      <a href="https://github.com/Roman3455" target="_blank" rel="noreferrer">@Roman3455</a>
+    </footer>
+  );
+}
+
+export default Footer
