90db19bc68e5c859431d01cbd16df8a504517059
Roman Smirnov <s.roman3455@gmail.com>
2026-01-21 19:52:30 +0300

    feat: add PageHeading components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header.jsx";
+import Home from "./components/Home.jsx";
 import Footer from "./components/footer/Footer.jsx";
 
 function App() {
   return (
     <>
       <Header />
+      <Home />
       <Footer />
     </>
   );
---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+import PageHeading from "./PageHeading.jsx";
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <PageHeading title="Enjoy your own collection!">
+        This is the place where your love of movies, games, comics, and anime
+        takes tangible form. We find, select, and bring the best figurines,
+        fan art, and limited editions so you can literally touch your favorite
+        universes.
+      </PageHeading>
+    </div>
+  );
+}
+
+export default Home
