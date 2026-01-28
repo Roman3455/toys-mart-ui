0a1af7cb4c0d1e4d2bf2cd01110ca6347d65fbc3
Roman Smirnov <s.roman3455@gmail.com>
2026-01-28 23:49:07 +0300

    feat: add hook to fetch backend data

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,24 @@
-import products from "../data/products.js";
 import PageHeading from "./PageHeading.jsx";
 import ProductListings from "./ProductListings.jsx";
 import apiClient from "../api/apiClient.js";
+import {useState, useEffect} from "react";
 
+// Hooks
 const Home = () => {
+
+  const [products, setProducts] = useState({});
+
+  // Run once when the component mounts
+  // Mounting is the process of creating and adding the component into DOM
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const fetchProducts = async () => {
+    const response = await apiClient.get("/products");
+    setProducts(response.data);
+  }
+
   return (
     <div className="max-w-[1152px] mx-auto px-6 py-8">
       <PageHeading title="Enjoy your own collection!">
