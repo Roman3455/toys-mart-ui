049995781374a2abc84737baa56a27bad6995d39
Roman Smirnov <s.roman3455@gmail.com>
2026-02-01 22:03:46 +0300

    feat: set useLoaderData in productsLoader()

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,54 +1,13 @@
 import PageHeading from "./PageHeading.jsx";
 import ProductListings from "./ProductListings.jsx";
 import apiClient from "../api/apiClient.js";
-import {useState, useEffect} from "react";
+import {useLoaderData} from "react-router-dom";
 
 // Hooks
-const Home = () => {
-
-  const [products, setProducts] = useState({});
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  // Run once when the component mounts
-  // Mounting is the process of creating and adding the component into DOM
-  useEffect(() => {
-    fetchProducts();
-  }, []);
-
-  const fetchProducts = async () => {
-    try {
-      setLoading(true);
-      const response = await apiClient.get("/products"); // Axios GET request
-      setProducts(response.data); // Update products state with fetched data
-    } catch (error) {
-      setError(
-        error.response?.data?.message ||
-        "Failed to fetch products. Please try again."
-      ); // Extract error message if available
-    } finally {
-      setLoading(false);
-    }
-  }
-
-  if (loading) {
-    return (
-      <div className="flex items-center justify-center min-h-screen">
-        <span className="text-xl font-semibold">Loading products...</span>
-      </div>
-    );
-  }
-
-  if (error) {
-    return (
-      <div className="flex items-center justify-center min-h-screen">
-        <span className="text-xl text-red-500">Error: {error}</span>
-      </div>
-    );
-  }
-
+export default function Home() {
+  const products = useLoaderData();
   return (
-    <div className="max-w-[1152px] mx-auto px-6 py-8">
+    <div className="max-w-6xl mx-auto px-6 py-8">
       <PageHeading title="Enjoy your own collection!">
         This is the place where your love of movies, games, comics, and anime
         takes tangible form. We find, select, and bring the best figurines,
@@ -60,4 +19,14 @@ const Home = () => {
   );
 }
 
-export default Home
+export async function productsLoader() {
+  try {
+    const response = await apiClient.get("/products"); // Axios GET Request
+    return response.data;
+  } catch (error) {
+    throw new Response(
+      error.message || "Failed to fetch products. Please try again.",
+      {status: error.status || 500}
+    );
+  }
+}
